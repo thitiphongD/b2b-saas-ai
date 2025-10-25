2394fce2d98443f3be39246f7f933e83a02b2119
Thitiphong Srisavat <80618380+thitiphongD@users.noreply.github.com>
2025-10-25 08:09:54 +0700

    fix type script

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
-import { ThemeToggle } from "@/components/theme-toggle";
-import { Button } from "@/components/ui/button";
-
+import { HeroHeader } from "@/components/header";
+import HeroSection from "@/components/hero-section";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello, B2B SaaS AI!</h1>
-      <Button>Click me</Button>
-      <ThemeToggle />
+      <HeroHeader />
+      <HeroSection />
     </div>
   );
 }
